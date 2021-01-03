ac05a33ba1899493453881143ff81bce98e99c78
elmasurgen <elmas.urgen@gmail.com>
2021-01-03 19:05:36 +0300

    1 file modified

--- src/components/PortfolioDetail.jsx
+++ src/components/PortfolioDetail.jsx
@@ -7,22 +7,19 @@ import { Route } from "react-router-dom";
 
 function PortfolioDetail(props) {
   const { personID } = useParams();
-  const person = Portfolio.filter(item => item._id === Number(personID));
-
-  const { theme } = useContext(ThemeContext);
-  const constrat = theme === "light" ? "dark" : "light";
-  return (
-    <div className="container">
-      <div className={`bg-${theme} text-${constrat}`}>
-        <hr />
+  const person = Portfolio.filter(item => item._id === Number(personID)).map(
+    item => (
+      <div className="container">
         <div className="d-flex justify-content-center">
-          <img src={`${person[0].imgURL}`} />
+          <img src={`${item.imgURL}`} />
         </div>
-        <h1 className="mb-3">{person[0].title}</h1>
-        <p>{person[0].detail}</p>
+        <h1 className="mb-3">{item.title}</h1>
+        <p>{item.detail}</p>
       </div>
-    </div>
+    )
   );
+
+  return <>{person}</>;
 }
 
 export default PortfolioDetail;
