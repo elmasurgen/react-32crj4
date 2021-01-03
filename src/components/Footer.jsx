ba7c82e3e44284e4d5251c487d952b670d123476
elmasurgen <elmas.urgen@gmail.com>
2021-01-03 19:35:19 +0300

    1 file modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 
-import ThemeContext from '../context/ThemeContext';
+import ThemeContext from "../context/ThemeContext";
 
 function Footer() {
-  
-  const {theme, toggleTheme} = useContext(ThemeContext)
+  const { theme, toggleTheme } = useContext(ThemeContext);
   const constrat = theme === "light" ? "dark" : "light";
-  return(
-    
-    <div className={`container-${theme} bg-${theme} text-${constrat}`}>
-         <footer className={`text-muted py-5 footer-${theme} bg-${theme} text-${constrat}`}>
+  return (
+    <footer
+      className={`text-muted py-5  bg-${theme} text-${constrat}`}
+    >
       <div className="container">
-         <button onClick={toggleTheme} className="btn btn-dark">{theme} Mode </button>
-            <p className="float-end mb-1">
+        <button onClick={toggleTheme} className="btn btn-dark">
+          {theme} Mode{" "}
+        </button>
+        <p className="float-end mb-1">
           <a href="#">Back to top</a>
         </p>
         <p className="mb-1">
@@ -28,7 +29,6 @@ function Footer() {
         </p>
       </div>
     </footer>
-    </div>
-  )
+  );
 }
 export default Footer;
